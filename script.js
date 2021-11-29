c911cf213c6e9846729a018409fd1f2231d7c89b
Сухомлинов Данил Игоревич <dsuhomlinov@bostonsd.ru>
2021-11-29 22:12:35 +0300

    lesson_05 final

--- script.js
+++ script.js
@@ -1,29 +1,47 @@
-const button1 = document.getElementById('but1'),
-    outPutForm = document.querySelector('.outPut'),
-    outPutText = document.createElement("p");
-
-button1.addEventListener('click', () => { getData() });
+/*
+To interact with program "npm start" and open index.html 
+*/
 
 
-const url = `http://localhost:5501`,
-    options = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        mode: 'no-cors',
-        connection: 'keep-alive'
+const url = `http://localhost:5501/`,
+    button1 = document.getElementById('but1');
+
+let history = '';
+
+function renderCatalog(responseArray, trgNode) {
+    for (e of responseArray) {
+        const liEl = document.createElement("li"),
+            location = e.name,
+            data = e.data;
+        if (e.type === 'link') {
+            liEl.insertAdjacentHTML('beforeend', `<a href="#">${location}</a>`);
+        }
+        else {
+            liEl.insertAdjacentText('beforeend', data);
+        }
+        liEl.addEventListener("click", (() => { getData(url, location); return true }));
+        trgNode.appendChild(liEl);
+    }
+    return true;
+}
+
+async function getData(url, location) {
+    const outPutForm = document.querySelector('.outPut');
+    history += location + '/';
+
+    while (outPutForm.firstChild) {
+        outPutForm.removeChild(outPutForm.firstChild);
     };
 
-// fetch(url)
-//     .then(response => response.text())
-//     .then(data => console.log(data));
+    let response = await fetch(url + history).then(r => r.text()),
+        responseArray = response.split('~#~').map(e => { return JSON.parse(e) });
 
-async function getData() {
-    const response = await fetch(url).then(r => r.text());
-    console.log(response);
-    outPutText.innerText = '';
-    outPutText.insertAdjacentText('afterbegin', response);
-    outPutForm.appendChild(outPutText);
-    return response;
+    renderCatalog(responseArray, outPutForm);
+
+    return true;
 }
+
+button1.addEventListener('click', () => { history = ''; getData(url, ''); return true });
+
+
+//resultArr.map(e => stringList += `<li><a href="http://localhost:5501/${e}">${e}</a></li>`);
